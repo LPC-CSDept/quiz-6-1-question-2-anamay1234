cc30c7f3bb1cf12cc5a239f781e859277c3b92b9
Anamay Singh <anaamaysingh@gmail.com>
2022-10-10 20:28:35 -0700

    6-1 Question 2

--- q2.cpp
+++ q2.cpp
@@ -33,3 +33,19 @@ void getInput(int &n1, int &n2, int &n3) {
 	cout << "Provide 3 integer values" << endl;
 	cin >> n1 >> n2 >> n3;
 }
+
+int findMin(int n1, int n2, int n3) {
+
+	
+	int min = n1;
+
+	if (min > n2) {
+		min = n2;
+	}
+
+	if (min > n3) {
+		min = n3;
+	}
+
+	return min;
+}
